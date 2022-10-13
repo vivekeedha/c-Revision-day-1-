c4f28758285f7fdeec1dba8df0e74f640c5f5ab2
vivekeedha <113223895+vivekeedha@users.noreply.github.com>
2022-10-13 10:58:02 +0530

    Add files via upload

---
+++ power of any number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ 
+using namespace std;
+  
+int main(){
+    int base, exp, i, result = 1;
+     
+ cout << "Enter base and exponent\n";
+    cin >> base >> exp;
+     
+    // Calculate base^exponent by repetitively multiplying base
+    for(i = 0; i < exp; i++){
+        result = result * base;
+    }
+      
+    cout << base << "^" << exp << " = " << result;
+     
+    return 0;
+}
